3a3b4df5e22dd5e48019794b1da2ef67505e51b5
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-05 00:14:06 -0400

    Algoritmo para calcular la edad en tiempo de ejecución con javascript

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,3 +10,34 @@ const descargarCV = (ruta) => {
     link.click();
     document.body.removeChild(link);
 };
+
+const getDaysDiff = (firstDate = new Date(`${(new Date).getFullYear()}/01/01`), secondDate = new Date()) => {
+    const firstDateInMs = firstDate.getTime();
+    const secondDateInMs = secondDate.getTime();
+
+    const differenceBtwDates = secondDateInMs - firstDateInMs;
+
+    const aDayInMs = 24 * 60 * 60 * 1000;
+
+    const daysDiff = Math.round(differenceBtwDates / aDayInMs);
+
+    return daysDiff;
+};
+
+const getCurrentAge = (firstDate = new Date('1993/07/23'), secondDate = new Date()) => {
+    const equalsMonth = (firstDate.getMonth() == secondDate.getMonth());
+
+    const validateBirthday = ((firstDate.getDate() == secondDate.getDate()) && equalsMonth);
+    const validateDay = (equalsMonth && (firstDate.getDate() < secondDate.getDate()));
+    const validateMonth = (firstDate.getMonth() < secondDate.getMonth());
+
+    const diffAnios = secondDate.getFullYear() - firstDate.getFullYear();
+
+    const edad = (validateBirthday || validateDay || validateMonth)  ? diffAnios : (diffAnios - 1);
+
+    document.querySelector('#edad').textContent = edad;
+};
+
+window.addEventListener('load', (e) => {
+    getCurrentAge();
+});
