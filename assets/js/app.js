0c3e711a4cfa9a9351d6ae10d6bb8fbd586562bf
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-12 17:26:01 -0400

    Sección portafolio corregida, se ajustó el tamaño de las imágenes del cpharma para que coincidiera con el resto de imagenes, se agregó la librería sweetalert2 para dar feedbacks al usuario y se usó código js para abrir el link de la página correspondiente

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,25 @@
+const experiencia = document.querySelector("#a-experiencia");
+
 //------------------------ Función para descargar el CV ------------------------
 const descargarCV = (ruta) => {
-    const link = document.createElement('a');
+    const link = document.createElement("a");
 
     link.target = "_blank";
     link.href = ruta;
-    link.download = (ruta === "./assets/documents/CV_Manuel_Henriquez.pdf") ? "CV_Manuel_Henriquez.pdf" : "CV_Manuel_Henriquez_I.pdf";
+    link.download =
+        ruta === "./assets/documents/CV_Manuel_Henriquez.pdf"
+            ? "CV_Manuel_Henriquez.pdf"
+            : "CV_Manuel_Henriquez_I.pdf";
 
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
 };
 
-const getDaysDiff = (firstDate = new Date(`${(new Date).getFullYear()}/01/01`), secondDate = new Date()) => {
+const getDaysDiff = (
+    firstDate = new Date(`${new Date().getFullYear()}/01/01`),
+    secondDate = new Date()
+) => {
     const firstDateInMs = firstDate.getTime();
     const secondDateInMs = secondDate.getTime();
 
@@ -24,44 +32,66 @@ const getDaysDiff = (firstDate = new Date(`${(new Date).getFullYear()}/01/01`), 
     return daysDiff;
 };
 
-const getYearsDiff = (firtDate = new Date("2018/01/01"), secondDate = new Date()) => {
+const getYearsDiff = (
+    firtDate = new Date("2018/01/01"),
+    secondDate = new Date()
+) => {
     const yearDiff = secondDate.getFullYear() - firtDate.getFullYear();
     return yearDiff;
 };
 
-const getCurrentAge = (firstDate = new Date('1993/07/23'), secondDate = new Date()) => {
-    const equalsMonth = (firstDate.getMonth() == secondDate.getMonth());
+const getCurrentAge = (
+    firstDate = new Date("1993/07/23"),
+    secondDate = new Date()
+) => {
+    const equalsMonth = firstDate.getMonth() == secondDate.getMonth();
 
-    const validateBirthday = ((firstDate.getDate() == secondDate.getDate()) && equalsMonth);
-    const validateDay = (equalsMonth && (firstDate.getDate() < secondDate.getDate()));
-    const validateMonth = (firstDate.getMonth() < secondDate.getMonth());
+    const validateBirthday =
+        firstDate.getDate() == secondDate.getDate() && equalsMonth;
+    const validateDay =
+        equalsMonth && firstDate.getDate() < secondDate.getDate();
+    const validateMonth = firstDate.getMonth() < secondDate.getMonth();
 
     const diffAnios = getYearsDiff(firstDate, secondDate);
 
-    const edad = (validateBirthday || validateDay || validateMonth) ? diffAnios : (diffAnios - 1);
+    const edad =
+        validateBirthday || validateDay || validateMonth
+            ? diffAnios
+            : diffAnios - 1;
 
-    document.querySelector('#edad').textContent = edad;
+    document.querySelector("#edad").textContent = edad;
 };
 
 const hideSection = (showSection) => {
-    const sections = document.querySelectorAll('.section');
-    
-    sections.forEach(section => {
-        if(!section.classList.contains(showSection))
-        {
-            section.classList.add('hidden');
+    const sections = document.querySelectorAll(".section");
+
+    sections.forEach((section) => {
+        if (!section.classList.contains(showSection)) {
+            section.classList.add("hidden");
         }
     });
 };
 
-window.addEventListener('load', (e) => {
-    document.querySelector("#a-experiencia").textContent = getYearsDiff();
+window.addEventListener("load", (e) => {
+    experiencia.textContent = getYearsDiff();
     getCurrentAge();
-    hideSection('service');
+    hideSection("portfolio");
 });
 
-document.addEventListener('click', (e) => {
+document.addEventListener("click", (e) => {
     const path = e.target.dataset.url;
+    const url = e.target.dataset.portfolioUrl;
 
-    if(path !== undefined) descargarCV(path);
+    if (path !== undefined) descargarCV(path);
+
+    if (url !== undefined && url !== "N/A") window.open(url, "_blank");
+    else if (url === "N/A") {
+        Swal.fire({
+            title: "Información",
+            text: "Este proyecto es privado y no tiene enlace disponible",
+            icon: "warning",
+            confirmButtonColor: "#3085d6",
+            confirmButtonText: "Aceptar",
+        });
+    }
 });
