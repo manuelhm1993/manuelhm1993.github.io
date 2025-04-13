93af23e13f955e9dad0a4a32d69cdac2aa329b8d
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-12 23:36:49 -0400

    Envío de correos con la api formsubmit terminado, se agregaron validaciones personalizadas ya que se est+a usando fetch

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
 const experiencia = document.querySelector("#a-experiencia");
+const contactForm = document.querySelectorAll('.contact .contact-form .form-control, .contact .contact-form .hidden input');
 
 //------------------------ Función para descargar el CV ------------------------
 const descargarCV = (ruta) => {
@@ -72,6 +73,53 @@ const hideSection = (showSection) => {
     });
 };
 
+const resetForm = () => {
+    contactForm.forEach((input) => {
+        if(input.type !== 'hidden') {
+            input.value = '';
+            input.setCustomValidity('');
+        }
+    });
+};
+
+const feedbackSubmitForm = (idioma) => {
+    //------------------------ Construir un objeto para swal
+    const objetoFeedback = {
+        titulo: '',
+        descripcion: '',
+        icono: 'success'
+    };
+
+    //------------------------ Dar un mensaje en el idioma correspondiente
+    if(idioma === 'espaniol') {
+        objetoFeedback.titulo = "Excelente";
+        objetoFeedback.descripcion = "Tu mensaje fue enviado con éxito";
+    }
+    else if(idioma === 'ingles') {
+        objetoFeedback.titulo = "Excellent";
+        objetoFeedback.descripcion = "Your message was sent successfully";
+    }
+
+    //------------------------ Crear el mensaje de alerta personalizado
+    Swal.fire({
+        title: objetoFeedback.titulo,
+        text: objetoFeedback.descripcion,
+        icon: objetoFeedback.icono,
+        confirmButtonColor: "#3085d6",
+        confirmButtonText: "Aceptar"
+    });
+
+    resetForm();
+};
+
+const fetchFormSubmit = async (url, parameters) => {
+    try {
+        const res = await fetch(url, parameters);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
 window.addEventListener("load", (e) => {
     experiencia.textContent = getYearsDiff();
     getCurrentAge();
@@ -79,21 +127,75 @@ window.addEventListener("load", (e) => {
 });
 
 document.addEventListener("click", (e) => {
-    const path = e.target.dataset.url;
-    const url = e.target.dataset.portfolioUrl;
+    const action = e.target.dataset;
+    const keys = ['url', 'portfolioUrl', 'submit'];
+    let key = '';
 
-    if (path !== undefined) descargarCV(path);
+    if(Object.keys(action).length > 0) {
+        for (const item of keys) {
+            if (action[item] !== undefined) {
+                key = item;
+                break;
+            }
+        }
+        switch(key) {
+            case 'url':
+                descargarCV(action[key]);
+                break;
+            case 'submit':
+                const data = {};
+        
+                contactForm.forEach((input) => {
+                    data[input.name] = input.value;
 
-    if ((url !== undefined) && (url !== "N/A")) {
-        window.open(url, "_blank");
-    }
-    else if (url === "N/A") {
-        Swal.fire({
-            title: "Información",
-            text: "Este proyecto es privado y no tiene enlace disponible",
-            icon: "warning",
-            confirmButtonColor: "#3085d6",
-            confirmButtonText: "Aceptar",
-        });
+                    if(input.value === '') {
+                        input.setCustomValidity("Este campo es obligatorio");
+                        input.reportValidity();
+                    }
+
+                    if(input.name === 'correo') {
+                        const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+                        if(!regex.test(input.value)) {
+                            input.setCustomValidity("El correo electrónico no es válido");
+                            input.reportValidity();
+                        }
+                    }
+                });
+
+                if(data.nombre != '' && data.correo != '' && data.mensaje != '' && data.asunto != '') {
+                    const parameters = {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify(data)
+                    };
+    
+                    feedbackSubmitForm(action.idioma);
+    
+                    // Hacer el fetch luego de 1 segundo del feedback
+                    setTimeout(() => {
+                        fetchFormSubmit(action[key], parameters);
+                    }, 1000);
+                }
+                else {
+
+                }
+                break;
+            case 'portfolioUrl':
+                if(action[key] !== 'N/A') {
+                    window.open(action[key], "_blank");
+                }
+                else {
+                    Swal.fire({
+                        title: "Información",
+                        text: "Este proyecto es privado y no tiene enlace disponible",
+                        icon: "warning",
+                        confirmButtonColor: "#3085d6",
+                        confirmButtonText: "Aceptar",
+                    });
+                }
+                break;
+        }
     }
 });
